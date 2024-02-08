63e19eccbfde8cf63ffb87274bf4579a350b554e
Rahman-Redoan <redoan.rahman@utexas.edu>
2024-02-08 00:13:33 -0600

    1. Fixed bugs with checking file dates, 2. Added satellite name, first epoch day and year and average frequency to the tracking records

--- update_data_source_information.py
+++ update_data_source_information.py
@@ -19,13 +19,15 @@ def verify_date(new_date):
                 res = bool(datetime.strptime(new_date, "%Y-%m-%d"))
         except ValueError:
                 res = False
-
         return res
 
 def convert_date_range(start_date, end_date, current_db_information):
-        if start_date is None or not verify_date(current_db_information["start_date"]):
+        is_a_date = True
+        if "start_date" in current_db_information:
+                is_a_date = verify_date(current_db_information["start_date"])
+        if start_date is None or is_a_date == False:
                 return start_date, end_date
-        
+                        
         if datetime.strptime(current_db_information["end_date"], "%Y-%m-%d") < datetime.strptime(start_date, "%Y-%m-%d"):
                 start_date = current_db_information["end_date"]
         elif datetime.strptime(current_db_information["start_date"], "%Y-%m-%d") > datetime.strptime(end_date, "%Y-%m-%d"):
@@ -49,12 +51,27 @@ def update_date_range(start_date, end_date, data_source, current_db_information)
 
 def isFileNotMerged(start_date, end_date, file_name, current_db_information):
         if start_date is None:
-                return (start_date not in  current_db_information
-                        or datetime.strptime( Path(file_name).stem, "%Y-%m-%d") < datetime.strptime(current_db_information["start_date"], "%Y-%m-%d") 
+                return ("start_date" not in  current_db_information
+                        or datetime.strptime( Path(file_name).stem, "%Y-%m-%d") < datetime.strptime(current_db_information["start_date"], "%Y-%m-%d")
                         or datetime.strptime( Path(file_name).stem, "%Y-%m-%d") > datetime.strptime(current_db_information["end_date"], "%Y-%m-%d"))
         else:
-                return ((datetime.strptime( Path(file_name).stem, "%Y-%m-%d") >= datetime.strptime(start_date, "%Y-%m-%d")) 
+                return ((datetime.strptime( Path(file_name).stem, "%Y-%m-%d") >= datetime.strptime(start_date, "%Y-%m-%d"))
                         and (datetime.strptime( Path(file_name).stem, "%Y-%m-%d") <= datetime.strptime(end_date, "%Y-%m-%d")))
+        
+def calculateRunningAvg(duration, frequencies, num_of_frequencies):
+        num_of_considered_frequencies = 0
+        frequency_sum = 0
+        while(num_of_frequencies > 0 and duration >= frequencies[num_of_frequencies - 1]):
+                duration -= frequencies[num_of_frequencies - 1]
+                frequency_sum += frequencies[num_of_frequencies - 1]
+                num_of_frequencies -= 1
+                num_of_considered_frequencies += 1
+
+        if duration > 0:
+                num_of_considered_frequencies += 1
+                frequency_sum += duration
+
+        return frequency_sum / num_of_considered_frequencies
 
 def update_JSPOC_database(start_date, end_date):
         current_db_information = {}
@@ -69,7 +86,7 @@ def update_JSPOC_database(start_date, end_date):
         if "JSPOC" not in current_db_information or "source_file_directory" not in current_db_information["JSPOC"]:
                 print("Error: Missing data source location. Exiting....")
                 return
-        
+
         if "created_database_name" not in current_db_information["JSPOC"] or current_db_information["JSPOC"]["created_database_name"] == "":
                 current_db_information["JSPOC"]["created_database_name"] = "updated_database.json"
 
@@ -78,13 +95,12 @@ def update_JSPOC_database(start_date, end_date):
         start_date, end_date = convert_date_range(start_date, end_date, current_db_information["JSPOC"])
         current_lowest_date = "9999-12-31"
         current_highest_date = "1990-01-01"
-
         try:
                 with open(current_db_information["JSPOC"]["created_database_name"]) as updated_database_file:
                         updated_json = json.load(updated_database_file)
         except FileNotFoundError:
                print("First time running for database for this filename. If this is not the first run, then please stop the run by cressing Ctrl + C and rename the old file according to the new name saved to the config file")
-        
+
         for file_name in file_list:
                 if (isFileNotMerged(start_date, end_date, file_name, current_db_information["JSPOC"])):
                         print(file_name)
@@ -106,32 +122,38 @@ def update_JSPOC_database(start_date, end_date):
                         except FileNotFoundError:
                                 source_frame["COUNTRY"] = "N/F"
                                 source_frame["LAUNCH"] = "N/F"
-                        
+
                         json_obj = source_frame.to_json(orient = 'records')
-                        for tuple in json.loads(json_obj):                                
+                        for tuple in json.loads(json_obj):
                                 if tuple["NORAD_CAT_ID"] not in updated_json:
                                         current_tuple = {
-                                                "epoch_year": tuple["epoch_year"],
-                                                "epoch_day": tuple["epoch_day"],
+                                                "name": tuple["name"],
+                                                "first_epoch_year": tuple["epoch_year"],
+                                                "first_epoch_day": tuple["epoch_day"],
+                                                "latest_epoch_year": tuple["epoch_year"],
+                                                "latest_epoch_day": tuple["epoch_day"],
                                         }
                                         updated_json[tuple["NORAD_CAT_ID"]] = current_tuple
                                         updated_json[tuple["NORAD_CAT_ID"]]["frequencies"] = [1]
-                                        updated_json[tuple["NORAD_CAT_ID"]]["n_data_change"] = 1
-                                elif updated_json[tuple["NORAD_CAT_ID"]]["epoch_year"] != tuple["epoch_year"] or updated_json[tuple["NORAD_CAT_ID"]]["epoch_day"] != tuple["epoch_day"]:
+                                        updated_json[tuple["NORAD_CAT_ID"]]["num_of_frequencies"] = 1
+                                        updated_json[tuple["NORAD_CAT_ID"]]["avg_frequency"] = 1
+                                elif updated_json[tuple["NORAD_CAT_ID"]]["latest_epoch_year"] != tuple["epoch_year"] or updated_json[tuple["NORAD_CAT_ID"]]["latest_epoch_day"] != tuple["epoch_day"]:
                                         updated_json[tuple["NORAD_CAT_ID"]]["frequencies"].append(1)
-                                        updated_json[tuple["NORAD_CAT_ID"]]["epoch_year"] = tuple["epoch_year"]
-                                        updated_json[tuple["NORAD_CAT_ID"]]["epoch_day"] = tuple["epoch_day"]
-                                        updated_json[tuple["NORAD_CAT_ID"]]["n_data_change"] += 1
+                                        updated_json[tuple["NORAD_CAT_ID"]]["latest_epoch_year"] = tuple["epoch_year"]
+                                        updated_json[tuple["NORAD_CAT_ID"]]["latest_epoch_day"] = tuple["epoch_day"]
+                                        updated_json[tuple["NORAD_CAT_ID"]]["avg_frequency"] = ((updated_json[tuple["NORAD_CAT_ID"]]["avg_frequency"] * updated_json[tuple["NORAD_CAT_ID"]]["num_of_frequencies"]) + 1) / (updated_json[tuple["NORAD_CAT_ID"]]["num_of_frequencies"] + 1)
+                                        updated_json[tuple["NORAD_CAT_ID"]]["num_of_frequencies"] += 1
                                 else:
-                                        updated_json[tuple["NORAD_CAT_ID"]]["frequencies"][updated_json[tuple["NORAD_CAT_ID"]]["n_data_change"] - 1] += 1
-                        
-                        
+                                        updated_json[tuple["NORAD_CAT_ID"]]["frequencies"][updated_json[tuple["NORAD_CAT_ID"]]["num_of_frequencies"] - 1] += 1
+                                        updated_json[tuple["NORAD_CAT_ID"]]["avg_frequency"] = ((updated_json[tuple["NORAD_CAT_ID"]]["avg_frequency"] * updated_json[tuple["NORAD_CAT_ID"]]["num_of_frequencies"]) + 1) / (updated_json[tuple["NORAD_CAT_ID"]]["num_of_frequencies"])
+
+
         update_date_range(current_lowest_date, current_highest_date, "JSPOC", current_db_information)
-        
+
         with open(current_db_information["JSPOC"]["created_database_name"], 'w') as f:
-                f.write(json.dumps(updated_json, indent = 2))
+                f.write(json.dumps(updated_json))
         with open('config.json', 'w') as f:
-                f.write(json.dumps(current_db_information, indent = 2))
+                f.write(json.dumps(current_db_information))
 
 # Main Function
 print("Please select any of the following datasets to update:")
